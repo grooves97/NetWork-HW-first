4afb2e688a1157faf8b5b8d66a0167c610bcbf4f
Аслан Нурсеитов <grooves97@gmail.com>
2019-06-19 16:05:42 +0600

    Создал визуальное представление поиска HostName и IP

---
+++ DnsApp/DnsApp/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DnsApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(addressTextBox.Text))
+            {
+                MessageBox.Show("Упс ты заполнил не верно IP или HostName!");
+                return;
+            }
+
+            try
+            {
+                var result = Dns.GetHostEntry(addressTextBox.Text);
+                hostNameTextBlock.Text = result.HostName;
+                ipListBox.ItemsSource = result.AddressList.ToList();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error: {exception.Message}");
+            }
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
